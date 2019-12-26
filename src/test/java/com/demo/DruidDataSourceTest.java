680e262a60e6f522771008a4a61d3aa07114230a
wuyk <413203315@qq.com>
2019-12-26 18:18:12 +0800

    增加角色权限shiro认证

---
+++ src/test/java/com/demo/DruidDataSourceTest.java
@@ -0,0 +1,35 @@
+package com.demo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+/**
+ * Created by WUYK on 2019-12-26.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {WebApplication.class})
+public class DruidDataSourceTest {
+
+    @Autowired
+    private DataSource dataSource;
+
+    @Test
+    public void contextLoads() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement("select * from user where id='1'");
+        ResultSet resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            String username = resultSet.getString("username");
+            System.out.println(username);
+        }
+    }
+}
